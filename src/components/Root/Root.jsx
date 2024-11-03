553c76c082a5d2c89ffcf9d39a50a5a398408876
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-11-03 20:48:52 +0600

    set up project-install router dom,tailwind,daisyUI

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,9 @@
+const Root = () => {
+    return (
+        <div>
+            <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, nihil.</p>
+        </div>
+    );
+};
+
+export default Root; 1
